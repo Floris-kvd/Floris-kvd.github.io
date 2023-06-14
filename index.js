fff44a5a49e84e5a9f671779198b709bba1379b5
Floris Kornelis van Dijken <badduckssb@gmail.com>
2023-06-14 14:50:33 +0200

    made it nicer

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// JavaScript code can be added here if you want to handle button actions or perform other operations.
+const toggleCheckbox = document.querySelector('.toggle-checkbox');
+const body = document.querySelector('body');
+const isDarkMode = localStorage.getItem('darkMode');
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  if (toggleCheckbox.checked) {
+    body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'true');
+  } else {
+    body.classList.remove('dark-mode');
+    localStorage.removeItem('darkMode');
+  }
+}
+
+// Function to apply dark mode on page load
+function applyDarkModePreference() {
+  if (isDarkMode) {
+    toggleCheckbox.checked = true;
+    body.classList.add('dark-mode');
+  }
+}
+
+// Event listener for toggle change
+toggleCheckbox.addEventListener('change', toggleDarkMode);
+
+// Apply dark mode preference on page load
+applyDarkModePreference();
